315382be24747a8ae0b2a6e7ce60c8191dced164
robcio89 <robcio89@gmail.com>
2015-04-14 09:08:36 +0200

    solution for question 1

---
+++ PQ1/merge_sort_code/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "FileReader.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Merge sort" << endl;
+
+	FileReader file;
+	file.hello();
+	getchar();
+	return 0;
+}
---
+++ PQ1/merge_sort_ut/Source.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+   /*The method is initializes the Google framework and must be called before RUN_ALL_TESTS */
+   ::testing::InitGoogleTest(&argc, argv);
+
+   /*RUN_ALL_TESTS automatically detects and runs all the tests defined using the TEST macro. 
+   It's must be called only once in the code because multiple calls lead to conflicts and, 
+   therefore, are not supported.
+   */ 
+   return RUN_ALL_TESTS();
+}
